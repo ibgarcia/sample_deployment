db5d692e759596270bd8f398714e38209527e8aa
Ishrael Garcia <i_garcia@g-n-s.co.jp>
2012-02-24 17:04:54 +0900

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleDeployment::Application.routes.draw do
+  resources :posts do
+  	resources :comments
+  end
+
   # get "home/index"
     root :to => "home#index"
   # The priority is based upon order of creation:
